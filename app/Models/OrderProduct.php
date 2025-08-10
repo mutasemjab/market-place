003e85ca84303fdb2fa3fabd8349a735676befbf
mutasem <mutasemms198@gmail.com>
2025-08-10 22:54:41 +0300

    first commit

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class OrderProduct extends Model
 {
     use HasFactory;
-
+    protected $guarded=[];
      public function order()
     {
         return $this->belongsTo(Order::class);
